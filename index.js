3960969f2b8c4fb28fd09ef46027629577375f74
ian <ianwatson115@gmail.com>
2020-01-30 20:08:43 -0500

    prompt the user two questions to receive answers

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const generate = require("./generateHTML");
+const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
+var prompt = inquirer.createPromptModule();
 
-const questions = [
-  "Please enter your Github Username",
-  
+const questions = [{
+	type: "input",
+	name: "username",
+	message: "Please enter your Github Username"
+},
+{
+	type: "list",
+	name: "color",
+	message: "What would you like to change the color to?",
+	choices: ["blue", "red", "pink", "green"]
+}
 ];
 
 axios.get("https://api.github.com")
 	.then((response) => {
-		console.log(response.data);
+		prompt(questions).then(answers => {
+			
+		});
 	})
 
 function writeToFile(fileName, data) {
- 
+
 }
+
+
 
 function init() {
 
