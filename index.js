d4bc2c89ae60a48858990bec0840a70aac82484c
ian <ianwatson115@gmail.com>
2020-02-10 16:07:55 -0500

    got a response from the api from entering a username

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const generate = require("./generateHTML");
 const fs = require("fs");
+const convertFactory = require("electron-html-to");
 const inquirer = require("inquirer");
 const axios = require("axios");
-var prompt = inquirer.createPromptModule();
+
+let conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
+});
 
 const questions = [{
 	type: "input",
@@ -14,23 +18,48 @@ const questions = [{
 	name: "color",
 	message: "What would you like to change the color to?",
 	choices: ["blue", "red", "pink", "green"]
-}
-];
+}];
 
-axios.get("https://api.github.com")
-	.then((response) => {
-		prompt(questions).then(answers => {
-			
+
+function getUser(response) {
+	axios
+		.get(`https://api.github.com/users/${response.username}`)
+		.then(result => {
+			profileSorter(result.data);
+		}).catch(err => {
+			console.log(`User not found`);
+			throw err;
 		});
-	})
-
-function writeToFile(fileName, data) {
-
 }
 
+function profileSorter(data) {
+	const star = data.starred_url.split(",");
+	const location = data.location;
+	const user = {
+		name: data.login,
+		profileImage: data.avatar_url,
+		githubLink: data.html_url,
+		location: location,
+		gMaps: `https://www.google.com/maps?q=${location}`,
+		blog: data.blog,
+		bio: data.bio,
+		publicRepos: data.public_repos,
+		followers: data.followers,
+		following: data.following,
+		stars: star.length
+	}
+	console.log(user);
+	// writeToPDF(user);
+}
 
+function writeToPDF (response) {
+	
+}
 
 function init() {
-
+	inquirer.prompt(questions).then(answers => {
+		const answerData = answers;
+		getUser(answers);
+	})
 }
-// init()
+init();
